9fd68ee0cf4500d418707a505ffcfd7e6ae9528a
UnicornDad8 <cecibenitezca@gmail.com>
2025-11-17 10:39:54 -0300

    Create first user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { Star } from 'lucide-react'
+import { SignIn } from "@clerk/clerk-react"
 
 const Login = () => {
   return (
@@ -29,7 +30,7 @@ const Login = () => {
       </div>
       {/* Right Side: Login Form */}
       <div className="flex flex-1 items-center justify-center p-6 sm:p-10">
-
+        <SignIn />
       </div>
     </div>
   )
