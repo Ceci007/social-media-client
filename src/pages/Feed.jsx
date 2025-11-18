316957c721ef1f3695001478633e3571261d6de5
UnicornDad8 <cecibenitezca@gmail.com>
2025-11-18 11:54:14 -0300

    Started Feed overall layout

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { dummyPostsData } from "../assets/assets"
+import Loading from "../components/Loading"
 
 const Feed = () => {
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchFeeds = async () => {
+    setFeeds(dummyPostsData);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchFeeds();
+  }, [])
+
   return (
-    <div>Feed</div>
+    <>
+      {
+        !loading ? (
+          <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
+            {/* Stories and Posts Lists */}
+            <div>
+              <h1>Stories here</h1>
+              <div className="p-4 space-y-6">
+                List of Posts
+              </div>
+            </div>
+            {/* Right Sidebar */}
+            <div>
+              <div>
+                <h1>Sponsored</h1>
+              </div>
+              <h1>Recent Messages</h1>
+            </div>
+          </div>
+        ) : <Loading />
+      }
+    </>
   )
 }
 
