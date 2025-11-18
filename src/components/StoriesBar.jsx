a9bc410c1e322a10099de2af5e3d1452b36314d2
UnicornDad8 <cecibenitezca@gmail.com>
2025-11-18 14:20:00 -0300

    Format timestamp on stories feed with moment package

--- src/components/StoriesBar.jsx
+++ src/components/StoriesBar.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
-import { dummyStoriesData } from "../assets/assets";
-import { Plus } from "lucide-react";
-
+import { dummyStoriesData } from "../assets/assets"
+import { Plus } from "lucide-react"
+import moment from "moment"
 
 const StoriesBar = () => {
   const [stories, setStories] = useState([]);
@@ -32,7 +32,7 @@ const StoriesBar = () => {
             <div key={index} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-linear-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-900 active:scale-95`}>
               <img src={story.user.profile_picture} alt={story.user.full_name} className="absolute size-8 top-3 left-3 z-10 rounded-full ring ring-gray-100 shadow" />
               <p className="absolute top-18 left-3 text-white/60 text-sm truncate max-w-24">{story.content}</p>
-              <p className="text-white absolute bottom-1 right-2 z-10 text-xs">{story.createdAt}</p>
+              <p className="text-white absolute bottom-1 right-2 z-10 text-xs">{moment(story.createdAt).fromNow()}</p>
             </div>
           ))
         }
